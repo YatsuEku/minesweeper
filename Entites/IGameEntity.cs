26b4f5ac59cfcf4405fe94f4f02700426cb62e83
YatsuEku <66636614+YatsuEku@users.noreply.github.com>
2020-12-25 11:01:23 +0100

    Add files via upload

---
+++ Entites/IGameEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saper.Entites
+{
+    public interface IGameEntity
+    {
+        public int DrawOrder { get; set; }
+
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+    }
+}
---
+++ Entites/Textures.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Saper
+{
+	public static class Textures
+	{
+		public static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+	}
+}
